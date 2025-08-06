7f8292e1ab5e94e10f33d4ca037ce76cbdb04b14
Lesego <146718089+Lesego-1@users.noreply.github.com>
2025-08-06 11:34:42 +0200

    22101

--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from .models import Restaurant
 
 def display_home_page_name(request):
-    render(request, "homepage.html", {"restaurant_name":Restaurant.name})
+    return render(request, "homepage.html", {"restaurant_name":Restaurant.objects.first().name})
