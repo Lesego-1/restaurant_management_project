20de78e8823870fad40f2457800572abd422275d
Lesego <146718089+Lesego-1@users.noreply.github.com>
2025-08-13 08:24:06 +0200

    31061

--- home/serializers.py
+++ home/serializers.py
@@ -4,4 +4,5 @@ from .models import MenuItem
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
-        fields = ['id', 'restaurant', 'name', '']
+        fields = ['id', 'restaurant', 'name', 'description', 'price', 'is_available', 'created_at']
+        read_only_fields = ['created_at']
