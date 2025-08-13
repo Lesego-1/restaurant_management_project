1d92092158c2ea4bf8f6302bd2c2e270471364eb
Lesego <146718089+Lesego-1@users.noreply.github.com>
2025-08-13 08:15:53 +0200

    31061

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path("home/", display_home_page_view, name="homepage"),
     path("feedback/", feedback_form_view, name="feedback_form"),
-    
+    path("menu_item/", menu_item_view, name="menu_item_view"),
 ]
