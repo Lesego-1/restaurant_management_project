68267bc660d1993f0d4a09087b4a13ae98b9a4b1
Lesego <146718089+Lesego-1@users.noreply.github.com>
2025-08-14 05:28:30 +0200

    31781

--- orders/models.py
+++ orders/models.py
@@ -3,8 +3,8 @@ from home.models import MenuItem
 from account.models import UserProfile
 
 class Orders(models.Model):
-    customer = models.OneToOneField(UserProfile, on_delete.CASCADE)
-    order_items = models.OneToManyField(MenuItem, on_delete.models.CASCADE)
+    customer = models.OneToOneField(UserProfile, on_delete=modelsCASCADE)
+    order_items = models.ManyToManyField(MenuItem, on_delete=models.CASCADE)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     order_status = models.BooleanField(default=False)  # Set default to False to show order is not done
 
