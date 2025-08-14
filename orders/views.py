994014d19faf251a5618f41de3ef89c55ab6656a
Lesego <146718089+Lesego-1@users.noreply.github.com>
2025-08-14 05:34:36 +0200

    31783

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view
+from .models import MenuItem
+from .serializer import MenuItemSerializer
 
-# Create your views here.
+@api_view(["GET"])
+def get_menu(request):
+    queryset = MenuItem
+    serializer = MenuItemSerializer(queryset, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
