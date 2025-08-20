ea964beb1622cbba00a63030539e066be9b2bd08
Lesego <146718089+Lesego-1@users.noreply.github.com>
2025-08-20 09:48:38 +0200

    35882

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class MenuItem(models.Model):
     restaurant = models.CharField(max_length=100, blank=True)
@@ -28,6 +29,14 @@ class RestaurantLocation(models.Model):
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class CartItem(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
