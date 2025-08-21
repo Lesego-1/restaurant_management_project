a7d6a7c047792a51974e4744f663bf9846609f0d
Lesego <146718089+Lesego-1@users.noreply.github.com>
2025-08-21 10:13:34 +0200

    36440

--- home/models.py
+++ home/models.py
@@ -41,3 +41,16 @@ class CartItem(models.Model):
 
     def __str__(self):
         return self.name
+
+class FAQBody(models.Model):
+    text = models.CharField(max_length=10000)
+
+    def __str__(self):
+        return self.text
+
+class FAQ(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.One
+
+    def __str__(self):
+        return self.title
