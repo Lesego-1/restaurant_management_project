254189d94ad5f562084f5bad70f1e0a94495b0ba
Lesego <146718089+Lesego-1@users.noreply.github.com>
2025-08-26 12:45:21 +0200

    37507

--- products/views.py
+++ products/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
-from .models import Item
+from .models import Item, Cart
 from .serializers import ItemSerializer
 
 '''
 NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
 '''
 
-# Create your views here.
 class ItemView(APIView):
 
     def get(self, request):
@@ -24,3 +22,9 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def cart_view(request):
+    user_cart = Cat.objects.filter(user=request.user)
+    if user_cart:
+        
+    return render(request, "cart.html")
