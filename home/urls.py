ac4e2751cfb317dd2f9cf7e769322e8f4e1b64f1
Lesego <146718089+Lesego-1@users.noreply.github.com>
2025-08-26 13:10:03 +0200

    37505

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("home/", display_home_page_view, name="homepage"),
     path("feedback/", feedback_form_view, name="feedback_form"),
     path("menu_item/", menu_item_view, name="menu_item_view"),
+    path("privacy_policy/", privacy_policy_view, name="privacy_policy")
 ]
