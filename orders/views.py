27aec577fd05169c918aab2a769dcc4caf37f9f5
Lesego <146718089+Lesego-1@users.noreply.github.com>
2025-08-27 08:41:50 +0200

    37503

--- orders/views.py
+++ orders/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response 
 from rest_framework.decorators import api_view
-from .models import MenuItem
+from .models import MenuItem, Order
 from .serializer import MenuItemSerializer
 
 @api_view(["GET"])
@@ -12,4 +12,5 @@ def get_menu(request):
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 def order_confirmation_view(request, pk):
-    order = Ord
+    order = Order.objects.filter(pk=pk)
+    if order.
