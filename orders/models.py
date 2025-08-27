d998e09966695d29ea092f12b1701b3c0467aa08
Lesego <146718089+Lesego-1@users.noreply.github.com>
2025-08-27 08:41:30 +0200

    37503

--- orders/models.py
+++ orders/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from home.models import MenuItem
 from account.models import UserProfile
 
-class Orders(models.Model):
+class Order(models.Model):
     customer = models.OneToOneField(UserProfile, on_delete=modelsCASCADE)
     order_items = models.ManyToManyField(MenuItem, on_delete=models.CASCADE)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
