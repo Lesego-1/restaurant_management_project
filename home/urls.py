076979065cdc337ccd657bfde5f9a4d7b8806923
Lesego <146718089+Lesego-1@users.noreply.github.com>
2025-09-08 09:21:04 +0200

    43112

--- home/urls.py
+++ home/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("privacy_policy/", privacy_policy_view, name="privacy_policy"),
     path("thank_you/", thank_you_page_view, name="thank_you"),
     path("our_story/", our_story_view, name="our_story"),
-    path("reservations/", reservations_view, name="reservations")
+    path("reservations/", reservations_view, name="reservations"),
+    path("our_team/", our_team_view, name="our_team"),
 ]
